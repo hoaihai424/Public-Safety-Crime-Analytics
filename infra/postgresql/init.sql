e2fa09cef2132d9037714b6967181a1f9dfc93d5
Pham Le Hoai Hai <hoaihai13579@gmail.com>
2025-11-07 21:33:10 +0700

    adjust init file for OLTP

--- infra/postgresql/init.sql
+++ infra/postgresql/init.sql
@@ -1,26 +1,21 @@
--- Create a keyspace for the crime data
-CREATE KEYSPACE IF NOT EXISTS crime_data WITH REPLICATION = {
-  'class': 'SimpleStrategy',
-  'replication_factor': 1
-};
-
--- Use the new keyspace
-USE crime_data;
+-- init database for postgresql
+CREATE DATABASE crime_data;
+\c crime_data;
 
 -- Create primary type table
 CREATE TABLE IF NOT EXISTS primary_type (
     id UUID PRIMARY KEY,
     name TEXT
-);
+);  
 
 -- Create crime table
 CREATE TABLE IF NOT EXISTS crime (
     id UUID PRIMARY KEY,
     iucr TEXT,
     fbi_code TEXT,
-    primary_type UUID,
+    primary_type UUID REFERENCES primary_type(id),
     description TEXT
-);
+);  
 
 CREATE INDEX IF NOT EXISTS crimes_iucr_idx ON crime (iucr);
 CREATE INDEX IF NOT EXISTS crimes_primary_type_idx ON crime (primary_type);
@@ -29,8 +24,8 @@ CREATE INDEX IF NOT EXISTS crimes_primary_type_idx ON crime (primary_type);
 CREATE TABLE IF NOT EXISTS location (
     id UUID PRIMARY KEY,
     location TEXT,
-    latitude DOUBLE,
-    longitude DOUBLE,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
     block TEXT,
     location_description TEXT
 );
@@ -51,18 +46,20 @@ CREATE INDEX IF NOT EXISTS patrol_units_beat_idx ON patrol_unit (beat);
 CREATE INDEX IF NOT EXISTS patrol_units_district_idx ON patrol_unit (district);
 
 -- Create case table
-CREATE TABLE IF NOT EXISTS case (
+CREATE TABLE IF NOT EXISTS case_report (
     id UUID PRIMARY KEY,
     case_number TEXT,
     arrest_made BOOLEAN,
     domestic BOOLEAN,
     incident_time TIMESTAMP,
-    location UUID,
-    patrol_unit UUID,
-    crime UUID
+    location UUID REFERENCES location(id),
+    patrol_unit UUID REFERENCES patrol_unit(id),
+    crime UUID REFERENCES crime(id)
 );
 
-CREATE INDEX IF NOT EXISTS cases_case_number_idx ON case (case_number);
-CREATE INDEX IF NOT EXISTS cases_crime_idx ON case (crime);
-CREATE INDEX IF NOT EXISTS cases_location_idx ON case (location);
-CREATE INDEX IF NOT EXISTS cases_patrol_unit_idx ON case (patrol_unit);
+CREATE INDEX IF NOT EXISTS cases_case_number_idx ON case_report (case_number);
+CREATE INDEX IF NOT EXISTS cases_crime_idx ON case_report (crime);
+CREATE INDEX IF NOT EXISTS cases_location_idx ON case_report (location);
+CREATE INDEX IF NOT EXISTS cases_patrol_unit_idx ON case_report (patrol_unit);
+
+
